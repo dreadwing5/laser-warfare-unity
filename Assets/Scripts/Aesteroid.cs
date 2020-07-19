d6ad8a4bdf354e757d684f65d1dc1b4849564a36
dreadwing5 <sachinspyk@outlook.com>
2020-07-19 18:21:29 +0530

    New Feature Added
    1. Aesteroid
    2.New Player Laser

---
+++ Assets/Scripts/Aesteroid.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aesteroid : MonoBehaviour
+{
+    [SerializeField] int Health = 500;
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] [Range(0,1)] float deathSoundVolume;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] float durationOfExplosion;
+    [SerializeField] int points;
+    [SerializeField] float moveSpeed = 10f;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, -moveSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
+        ProcessHits(damageDealer);
+
+    }
+
+    private void ProcessHits(DamageDealer damageDealer)
+    {
+        Health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<GameSession>().AddToScore(points);
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(deathVFX, transform.position, Quaternion.identity) as GameObject;
+        AudioSource.PlayClipAtPoint(deathSound, Camera.main.transform.position, deathSoundVolume);
+        Destroy(explosion, durationOfExplosion);
+    }
+
+
+}
---
+++ Assets/Scripts/AesteroidSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AesteroidSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject aesteroidPrefab;
+    [SerializeField] float respawnTime = 100f;
+    private Vector2 screenBound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBound = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(AesteroidWave());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator AesteroidWave()
+
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+        }
+
+        
+    }
+
+
+    public void spawnEnemy()
+    {
+        GameObject aesteroid = Instantiate(aesteroidPrefab) as GameObject;
+        aesteroid.transform.position = new Vector2(Random.Range(-screenBound.x, screenBound.x), screenBound.y * 2);
+
+    }
+
+}
+
+  
